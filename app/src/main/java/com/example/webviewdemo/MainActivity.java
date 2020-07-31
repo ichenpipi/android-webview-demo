dce9ced023ee96fd21e4116d4045d4832c15a8e9
陈皮皮 <952157129@qq.com>
2020-08-01 01:52:37 +0800

    update

--- app/src/main/java/com/example/webviewdemo/MainActivity.java
+++ app/src/main/java/com/example/webviewdemo/MainActivity.java
@@ -13,7 +13,7 @@ import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    // public WebView webView = null; // WebView 实例
+    // private WebView webView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +28,20 @@ public class MainActivity extends AppCompatActivity {
         this.createWebView();
     }
 
-    /*
-     * 创建 WebView
-     */
+    /* 创建 WebView 实例 */
     @SuppressLint("SetJavaScriptEnabled")
     private void createWebView() {
-        // 创建 WebView 实例并通过 id 绑定我们刚创建的布局
+        // 创建 WebView 实例并通过 id 绑定我们刚在布局中创建的 WebView 标签
         // 这里的 R.id.webview 就是 activity_main.xml 中的 WebView 标签的 id
-        // this.webView = (WebView) findViewById(R.id.webview);
         final WebView webView = (WebView) findViewById(R.id.webview);
+
         // 设置 WebView 允许执行 JavaScript 脚本
         webView.getSettings().setJavaScriptEnabled(true);
+
         // 确保跳转到另一个网页时仍然在当前 WebView 中显示
         // 而不是调用浏览器打开
         webView.setWebViewClient(new WebViewClient());
+
         // 设置 WebView 的按键监听器，覆写监听器的 onKey 函数，对返回键作特殊处理
         // 当 WebView 可以返回到上一个页面时回到上一个页面
         webView.setOnKeyListener(new View.OnKeyListener() {
@@ -54,14 +54,14 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
         // 加载指定网页
+        // String url = "https://chenpipi.cn";
         String url = "http://192.168.137.1:7456";
         webView.loadUrl(url);
     }
 
-    /*
-     * 接管返回键
-     */
+    /* 接管返回键 */
     /* @Override
      public void onBackPressed() {
          if (this.webView.canGoBack()) {
